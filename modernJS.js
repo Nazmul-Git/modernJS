8d55a01fd8af8d7741550c491c356f748b80600f
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-12-19 01:33:25 +0600

    We can also export default anonymous function and import by any name.

--- modernJS.js
+++ modernJS.js
@@ -1,6 +1,9 @@
 // import
 import {addToCart, shoppingCart, totalPrice as tp, tq} from './module.js'
+import addCart from './module.js'
 console.log('import module');
 addToCart('phone', 2);
 
 console.log(tp, tq, shoppingCart)
+
+addCart('mouse', 1);
--- module.js
+++ module.js
@@ -13,3 +13,9 @@ const totalPrice= 278;
 const totalQuantity= 24;
 
 export {totalPrice, totalQuantity as tq, shoppingCart}
+
+
+export default (product, quantity)=>{
+    cart.push({product, quantity});
+    console.log(`${quantity} ${product}`);
+}

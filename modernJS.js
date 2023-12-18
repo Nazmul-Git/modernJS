1a837427542ce6357bc1020cd5597e3b9a1166aa
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-12-19 02:55:22 +0600

    After fetching data function return a promise and get promise data we have to use then or await(best)..

--- modernJS.js
+++ modernJS.js
@@ -18,23 +18,41 @@
 // console.log(cart);
 
 
-// // Call a function you type is Async
-// console.log("await inside function");
-// const postData = async () => {
-//   console.log("start fetching");
-//   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-//   const data = await res.json();
-//   console.log(data);
-// };
-// postData();
-// console.log("end fetching");
 
 
 ////////////////////////////
 
-console.log('await outside function behave as synchronous')
-console.log("start fetching");
-const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-const data = await res.json();
-console.log(data);
-console.log("end fetching");
+// console.log('await outside function behave as synchronous/block the next line code.')
+// console.log("start fetching");
+// const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+// const data = await res.json();
+// console.log(data);
+// console.log("end fetching");
+
+
+//////////////////////////////////////////
+
+// Call a function you type is Async
+// console.log("await inside function");
+const postData = async () => {
+  console.log("start fetching");
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
+  console.log(data);
+  return {title:data.at(-1).title, text: data.at(-1).body};
+};
+// const lastPost= postData();
+// console.log(lastPost); // Promise {<pending>}
+
+// lastPost.then(last=>console.log(last)); //{title: 'at nam consequatur ea labore ea harum', text: 'cupiditate quo est a modi nesciunt soluta\nipsa vol…nam et distinctio eum\naccusamus ratione error aut'}
+
+const lastPost2= await postData();
+console.log(lastPost2); //{title: 'at nam consequatur ea labore ea harum', text: 'cupiditate quo est a modi nesciunt soluta\nipsa vol…nam et distinctio eum\naccusamus ratione error aut'}
+
+
+
+
+
+
+
+// console.log("end fetching");

60129261de83720f55d24e2f241219725c9cef35
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-12-19 02:26:40 +0600

    Note that await outside function behave as synchronous.

--- modernJS.js
+++ modernJS.js
@@ -1,9 +1,40 @@
-// import
-import {addToCart, shoppingCart, totalPrice as tp, tq} from './module.js'
-import addCart from './module.js'
-console.log('import module');
-addToCart('phone', 2);
+// // import
+// import {
+//   addToCart,
+//   shoppingCart,
+//   totalPrice as tp,
+//   tq,
+//   cart,
+// } from "./module.js";
+// import addCart from "./module.js";
+// console.log("import module");
+// addToCart("phone", 2);
 
-console.log(tp, tq, shoppingCart)
+// console.log(tp, tq, shoppingCart);
 
-addCart('mouse', 1);
+// addCart("mouse", 1);
+// addCart("keyboard", 3);
+// addCart("laptop", 5);
+// console.log(cart);
+
+
+// // Call a function you type is Async
+// console.log("await inside function");
+// const postData = async () => {
+//   console.log("start fetching");
+//   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+//   const data = await res.json();
+//   console.log(data);
+// };
+// postData();
+// console.log("end fetching");
+
+
+////////////////////////////
+
+console.log('await outside function behave as synchronous')
+console.log("start fetching");
+const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+const data = await res.json();
+console.log(data);
+console.log("end fetching");
--- module.js
+++ module.js
@@ -2,7 +2,7 @@
 console.log('export module'); 
 
 const shoppingCart=10;
-const cart= [];
+export const cart= [];
 
 export const addToCart=(product, quantity)=>{
     cart.push({product, quantity});
